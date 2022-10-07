6da60d29499ffba676de34c62f04226ebeec29ab
Namitha P R <namitha1997@gmail.com>
2022-10-07 12:54:52 +0530

    Update index.tsx
    comment date time pickers

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 import WheelPicker from './src/WheelPicker'
-import TimePicker from './src/TimePicker'
-import DatePicker from './src/DatePicker'
+//import TimePicker from './src/TimePicker'
+//import DatePicker from './src/DatePicker'
 
-export { WheelPicker, TimePicker, DatePicker }
+export { WheelPicker}
